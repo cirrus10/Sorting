071b755d3e86ec2b6719ab716ef3db0eda2f4c0d
Madhura Kulkarni <madhurakulkarni@Madhuras-MacBook-Pro.local>
2016-12-19 14:04:05 -0800

    BubbleSort

--- BubbleSortAlgo.java
+++ BubbleSortAlgo.java
@@ -5,13 +5,13 @@ public class BubbleSortAlgo {
 		int length =  array.length;
 		for (int i = 0; i< length; i++)
 		{
-			for(int j = 0; j<length; j++)
+			for(int j = 0; j<length-1; j++)
 			{
-				if(array[i]<array[j])
+				if(array[j]>array[j+1])
 				{
-					int temp = array[i];
-					array[i] = array[j];
-					array[j] = temp;
+					int temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
 				}
 			}
 		}
