ff2172176744a2a52c8aff45fc3f3cfdb75537f3
Madhura Kulkarni <madhurakulkarni@Madhuras-MacBook-Pro.local>
2016-12-19 21:28:22 -0800

    QuickSort

---
+++ QuickSortAlgo.java
@@ -0,0 +1,44 @@
+
+public class QuickSortAlgo {
+	 static int[] array;
+	
+	
+	public static void sort(int[] input, int l, int h)
+	{
+		array = input;
+		if(l < h)
+		{
+			int pivot = array[h];
+			int pIndex = l;
+			for(int i = l; i<h; i++)
+			{
+				if(array[i]<= pivot)
+				{
+					swap(pIndex,i);
+					pIndex = pIndex++;
+				}
+			}
+			swap(pIndex, h);
+					
+			sort(array, l, pIndex-1);
+			sort(array, pIndex+1, h);
+		}
+	}
+
+	public static void swap(int a, int b){
+		int temp = array[a];
+		array[a] = array[b];
+		array[b] = temp;
+	}
+	
+	public static void main(String args[])
+	{
+		int[] input = {20,22,24,26,28,18};
+		sort(input, 0, input.length -1);
+		for(int k: array)
+		{
+			System.out.print(k+" ");
+		}
+	}
+
+}
